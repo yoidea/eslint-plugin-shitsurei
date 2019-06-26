8e13a415ac9600d297336bd2669d381c96265956
Yoidea <yoidea.cc@gmail.com>
2019-06-27 01:17:50 +0900

    :sparkles: Add a config file

--- configs/recommended.js
+++ configs/recommended.js
@@ -1,14 +1,10 @@
-{
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "es6": true
-  },
-  "rules": {
+module.exports = {
+  plugins: ["shitsurei"],
+  rules: {
     "great-var": 2,
     "no-break": 2,
     "no-else": 2,
     "no-for-test": 2,
     "no-no-return": 2
   }
-}
+};
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 module.exports = {
-    rules: {
-        "great-var": require("./rules/great-var"),
-        "no-break": require("./rules/no-break"),
-        "no-else": require("./rules/no-else"),
-        "no-for-test": require("./rules/no-for-test"),
-        "no-no-return": require("./rules/no-no-return")
-    },
+  rules: {
+    "great-var": require("./rules/great-var"),
+    "no-break": require("./rules/no-break"),
+    "no-else": require("./rules/no-else"),
+    "no-for-test": require("./rules/no-for-test"),
+    "no-no-return": require("./rules/no-no-return")
+  },
+  configs: {
+    recommended: require("./configs/recommended")
+  }
 }
